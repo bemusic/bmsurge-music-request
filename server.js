66c6353c93f671ecdc406bc1fb9a2ef490c830cf
Glitch (hello-express) <dtinth@spacet.me>
2019-09-07 04:33:41 +0000

    🐩🎅 Checkpoint ./server.js:577339/2578 ./package.json:577339/1522

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ if (process.env.GIT_EMAIL) {
 const express = require('express');
 const basicAuth = require('express-basic-auth')
 const app = express();
-require('longjohn');
+const algoliasearch = require('algoliasearch')
+const algolia = algoliasearch('GDXU17NOOL', process.env.ALGOLIA_ADMIN_API_KEY)
+require('longjohn')
 
 const authenticated = basicAuth({
   users: { admin: process.env.ADMIN_PASSWORD },
@@ -13,19 +15,38 @@ const authenticated = basicAuth({
   realm: 'bmsurge-music-request',
 })
 
-app.use(require('body-parser').json())
+app.use(require('body-parser').json({ limit: '8mb' }))
 app.use(express.static('public'))
 
 app.post('/discord', authenticated, async function(req, res, next) {
   try {
+    const index = algolia.initIndex('songs')
+    const result = await index.search({ query: req.body.content })
+    const hits = result.hits
+    const x = 
+    console.log(result.hits)
     res.send('meow!')
   } catch (e) {
     next(e)
   }
 })
+
 app.put('/songlist', authenticated, async function(req, res, next) {
   try {
-    res.send('meow!')
+    const index = algolia.initIndex('songs')
+    const data = req.body.map(s => {
+      return {
+        md5: s.md5,
+        songId: s.songId,
+        genre: s.genre,
+        title: s.title,
+        artist: s.artist,
+        event: s.event,
+        objectID: s.songId,
+      }
+    })
+    await index.addObjects(data)
+    res.send('Indexed ' + data.length + ' items')
   } catch (e) {
     next(e)
   }
