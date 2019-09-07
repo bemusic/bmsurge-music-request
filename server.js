aafa8b83940cc97690f84f81e83441b54eae79c8
Glitch (hello-express) <dtinth@spacet.me>
2019-09-07 05:33:40 +0000

    🔑🍽 Checkpoint ./server.js:577339/3598

--- server.js
+++ server.js
@@ -19,12 +19,28 @@ const authenticated = basicAuth({
 app.use(require('body-parser').json({ limit: '8mb' }))
 app.use(express.static('public'))
 
+const logAnalytics = event => {
+  return axios.get(`https://api.amplitude.com/httpapi`, {
+    params: {
+      api_key: process.env.AMPLITUDE_API_KEY,
+      event: JSON.stringify(event)
+    }
+  })
+}
+
 app.post('/discord', authenticated, async function(req, res, next) {
   try {
     const index = algolia.initIndex('songs')
-    const result = await index.search({ query: req.body.content, hitsPerPage: 1000 })
+    const query = req.body.content
+    const result = await index.search({ query, hitsPerPage: 1000 })
     const hits = result.hits
+    const userId = req.body.userId
     if (!hits.length) {
+      logAnalytics({
+        user_id: userId,
+        event_type: 'Request miss',
+        event_properties: { query }
+      })
       res.send(
         `Did not find any song matching your query, sorry...`
       )
@@ -38,6 +54,18 @@ app.post('/discord', authenticated, async function(req, res, next) {
         username: req.body.username,
         songId: song.songId,
         content: req.body.content
+      })
+      logAnalytics({
+        user_id: userId,
+        event_type: 'Request hit',
+        event_properties: {
+          query,
+          songId: song.songId,
+          title: song.title,
+          artist: song.artist,
+          genre: song.genre,
+          event: song.event,
+        }
       })
       res.send(`${response.data}\n(Song search powered by Algolia)`)
     } catch (e) {
